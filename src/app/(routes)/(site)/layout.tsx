2a9a8445d51a4e507704b75e155e807fd82f767f
theresa-o <okorotheresa19@gmail.com>
2025-03-01 20:20:40 +0100

    removed unused value from layout

--- src/app/(routes)/(site)/layout.tsx
+++ src/app/(routes)/(site)/layout.tsx
@@ -1,14 +1,7 @@
-import Footer from "@/app/components/footer/Footer";
-
 export default function SiteLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div>
-      {children}
-      
-    </div>
-  );
+  return <div>{children}</div>;
 }
