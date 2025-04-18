04eeb73cce6a25b291be5be1d8bd8ccf5c3d5ce8
theresa-o <okorotheresa19@gmail.com>
2025-04-18 22:51:13 +0100

    Updated search to use SSR

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-
 import { Suspense } from "react";
 import SearchResults from "../../components/filtering/searchResults";
-import { useSearchParams } from "next/navigation";
 import Search from "@/app/components/filtering/search";
 
-export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("query") || "";
-  return (
+export default function SearchPage({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || "";  return (
     <div className="container mx-auto py-8">
       <div className="flex justify-center pb-4">
         <div className="w-full max-w-2xl">
+          
           <Search />
         </div>
       </div>
